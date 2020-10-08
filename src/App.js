4a4bdadb60aac93552e1e0c8120645f785b29413
Mohamed Saleh Mohamed <72348177+Mo-Saleh-Mo@users.noreply.github.com>
2020-10-08 05:01:56 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@
 import React from 'react'
 
 const App = () => {
-  return ( <
-    div className = "grid-container" >
-    <
-    header >
-    <
-    a href = "/" > React Shopping Cart < /a> < /
-    header > <
-    main >
-    the min <
-    /main> <
-    footer >
-    All Right Is Resere <
-    /footer> < /
-    div >
+  return ( 
+    <div className = "grid-container" >
+      <header>
+        <a href="/" > React Shopping Cart </a> 
+      </header>
+      <main >
+        the min
+      </main>
+      <footer>
+        All Right Is Resere 
+      </footer> 
+    </div>
   );
 }
 
